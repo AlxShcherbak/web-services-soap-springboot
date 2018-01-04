ae981472daab2822a18d5db1a17a0ef506cc8942
alxshcherbak <alxshcherbak@bitbucket.org>
2018-01-04 15:45:33 +0200

    remake producer to clear spring mvc

--- server/src/main/java/com/oshcherbak/config/AppConfig.java
+++ server/src/main/java/com/oshcherbak/config/AppConfig.java
@@ -1,13 +1,11 @@
 package com.oshcherbak.config;
-
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.ws.config.annotation.EnableWs;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
@@ -15,16 +13,17 @@ import static com.oshcherbak.config.Constants.*;
 
 @Configuration
 @EnableWs
+@EnableWebMvc
 @ComponentScan("com.oshcherbak")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context){
-        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-        servlet.setApplicationContext(context);
-        servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean(servlet, LOCATION_URI + MAPPING_ROOT);
-    }
+//    @Bean
+//    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context){
+//        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+//        servlet.setApplicationContext(context);
+//        servlet.setTransformWsdlLocations(true);
+//        return new ServletRegistrationBean(servlet, LOCATION_URI + MAPPING_ROOT);
+//    }
 
     @Bean(name = "entities")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema){
